c77f134d59c5e320135fdf051f9d6c503f75ebd2
sangilyoon-dev <sangilyoon.dev@gmail.com>
2021-07-23 22:52:53 +0900

    0723

--- 윤상일/BOJ/2884_알람 시계.kt
+++ 윤상일/BOJ/2884_알람 시계.kt
@@ -1,13 +1,13 @@
 fun main() {
-    val input = readLine()!!.split(" ").map { it.toInt() }.toMutableList()
+    val input = readLine()!!.split(" ").map { it.toInt() }
     var h:Int = 0
     var m:Int = 0
-    if (input.get(1) < 45) {
-        if (input.get(0) == 0) h = 23 else h = input.get(0) - 1
-        m = input.get(1) + 15
+    if (input[1] < 45) {
+        if (input[0] == 0) h = 23 else h = input[0] - 1
+        m = input[1] + 15
     } else {
-        h = input.get(0)
-        m = input.get(1) - 45
+        h = input[0]
+        m = input[1] - 45
     }
     print("$h $m")
 }
