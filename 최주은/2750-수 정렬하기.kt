79d0b0480055c6a948aad4e0e785c6ac6bf4fe16
Jueun Choi <ilutoo@naver.com>
2021-07-26 02:55:08 +0900

    Create 2750-수 정렬하기.kt

---
+++ 최주은/2750-수 정렬하기.kt
@@ -0,0 +1,27 @@
+import java.util.Scanner
+
+fun main(args: Array<String>){
+    val sc:Scanner = Scanner(System.`in`)
+    val count = sc.nextInt()
+    val nums = IntArray(count)
+
+    for(i in 0 until count){
+        nums[i] = sc.nextInt()
+    }
+
+    bubbleSort(nums, count)
+
+    nums.forEach( {println("$it ")})
+}
+
+fun bubbleSort(nums:IntArray, count:Int){
+    for(i in 0 until count){
+        for (j in 0 until count-i-1){
+            if (nums[j] > nums[j+1]){
+                val temp:Int = nums[j+1]
+                nums[j+1] = nums[j]
+                nums[j] = temp
+            }
+        }
+    }
+}
