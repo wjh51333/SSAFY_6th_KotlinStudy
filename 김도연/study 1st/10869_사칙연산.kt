2e9fda59ccdafc8f9828fb599b60870347c40d76
Dyeon Kim <45352173+DyeonKim@users.noreply.github.com>
2021-07-26 23:25:00 +0900

    Create 10869_사칙연산.kt

---
+++ 김도연/study 1st/10869_사칙연산.kt
@@ -0,0 +1,15 @@
+import java.util.*
+
+// b는 0이 아니라 가정
+fun main() {
+    val sc:Scanner = Scanner(System.`in`)
+
+    val a = sc.nextInt()
+    val b = sc.nextInt()
+
+    println(a + b)
+    println(a - b)
+    println(a * b)
+    println(a / b)
+    println(a % b)
+}
