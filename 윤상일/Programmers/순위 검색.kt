8206640283e0ef59701ab9396f0ab602dd6ac042
SangIl Yoon <76988389+sangilyoon-dev@users.noreply.github.com>
2021-08-04 23:52:17 +0900

    Create 순위 검색.kt

---
+++ 윤상일/Programmers/순위 검색.kt
@@ -0,0 +1,10 @@
+class Solution {
+	fun solution(info: Array<String>, query: Array<String>): IntArray {
+		val answer: IntArray = intArrayOf()
+		var table: Array<List<String>>
+		info.forEach {
+			it.split(" ")
+		}
+		return answer
+	}
+}
