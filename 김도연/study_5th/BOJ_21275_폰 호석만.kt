4945c81e829c95b29cf500b5d52c17bc92a30b0a
Dyeon Kim <45352173+DyeonKim@users.noreply.github.com>
2021-08-26 01:16:05 +0900

    Update BOJ_21275_폰 호석만.kt

--- 김도연/study_5th/BOJ_21275_폰 호석만.kt
+++ 김도연/study_5th/BOJ_21275_폰 호석만.kt
@@ -1,21 +1,20 @@
+val digit = arrayListOf('0', '1', '2', '3', '4', '5', '6', '7', '8', '9'
+                        , 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h' ,'i', 'j'
+                        , 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't'
+                        , 'u', 'v', 'w', 'x', 'y', 'z')
+
 fun main() {
     val input = readLine()!!.split(" ")
     val inputA = input[0]
     val inputB = input[1]
 
-    // a,b의 범위 : 각자의 가장 큰 숫자보다 큰 수
-    // 조건 1. a != b --> a == b의 경우만 존재하면 impossable
-    // 조건 2. a와 b 만족 조합이 2개 이상 --> multiple
-    // 조건 3. 2 <= a, b <= 36
-    // 조건 4. x <= Long.MAX_VALUE
-
-    val maxA = getMaxChar(inputA) - 'W'
-    val maxB = getMaxChar(inputB) - 'W'
+    val maxA = getMax(inputA)
+    val maxB = getMax(inputB)
 
     if (maxA == 35 && maxA == maxB) {
         println("Impossible")
     } else {
-        var x: Long = 0; var a = 0; var b = 0; var count = 0
+        var a = 0; var b = 0; var count = 0
 
         for (i in (maxA+1)..36) {
             for (j in (maxB+1)..36) {
@@ -25,31 +24,30 @@ fun main() {
                 var aX = nToDec(inputA, i)
                 var bX = nToDec(inputB, j)
 
-                if (aX > Long.MAX_VALUE || bX > Long.MAX_VALUE)
-                    continue
                 if (aX == bX) {
-                    x = aX; a = i; b = j
+                    if (aX > Long.MAX_VALUE)
+                        continue
+                    a = i; b = j
                     count++
                 }
             }
         }
 
-        if (count == 1)
-            println("${x} ${a} ${b}")
-        else if (count > 1)
-            println("Multiple")
-        else
-            println("Impossible")
+        when {
+            count == 1 -> println("${nToDec(inputA, a)} $a $b")
+            count > 1 -> println("Multiple")
+            count == 0 -> println("Impossible")
+        }
     }
 }
 
-fun getMaxChar(s: String): Char {
+fun getMax(s: String): Int {
     var max = s[0]
-    for (i in 1 until s.length-1) {
+    for (i in 1 until s.length) {
         if (s[i] > max)
             max = s[i]
     }
-    return max
+    return digit.indexOf(max)
 }
 
 fun nToDec(s: String, n: Int): Long {
@@ -57,15 +55,8 @@ fun nToDec(s: String, n: Int): Long {
 
     for (i in s.indices) {
         result *= n
-        result += nToDec(s[i], i)
+        result += digit.indexOf(s[i])
     }
 
     return result
-}
-
-fun nToDec(c: Char, n :Int): Int {
-    return if (c in '0'..'9')
-        c - '0'
-    else
-        c - 'W'
 }
